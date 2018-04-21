88cbb502ccbe5d104174b70d0f1e1c90c7073f6d
Tad Preston <tadpreston@gmail.com>
2018-04-21 12:31:41 -0500

    Adding the appropriate definitions for materialize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require materialize
+//= require rails-ujs
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
